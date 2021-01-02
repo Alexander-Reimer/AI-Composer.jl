bacb062556f896d30736aa2c2f7e452956e48576
A_R_102 <72086650+AR102@users.noreply.github.com>
2021-01-02 17:38:03 +0100

    Update main_backpropagation.jl

--- main_backpropagation.jl
+++ main_backpropagation.jl
@@ -3,9 +3,9 @@
     println("   Importing \"PyPlot\"...")
     using PyPlot
     println("   \"PyPlot\" imported.")
-    println("   Importing \"JSON\"...")
-    using JSON
-    println("   \"JSON\" imported.")
+    println("   Importing \"JLD\"...")
+    using JLD
+    println("   \"JLD\" imported.")
     println("Packages imported.")
 
 #Network structure
@@ -15,12 +15,12 @@
         netinput::Float64
         activision::Float64
         δ::Float64
-        bias::Float64
     end
     #Layer
     #A Layer consists of the neurons it contains and a bias
     mutable struct Layer
         neurons::Array{Neuron}
+        bias::Float64
     end
 
     #Weights
@@ -52,13 +52,9 @@
         for i = 1:length(neuronsInLayer)
             neurons = []
             for i2 = 1:neuronsInLayer[i]
-                if rndgen
-                    push!(neurons, Neuron(0.0, 0.0, 0.0, rand()))
-                else
-                    push!(neurons, Neuron(0.0, 0.0, 0.0, 0.0))
-                end
+                push!(neurons, Neuron(0.0, 0.0, 0.0))
             end
-            push!(allLayers, Layer(neurons))
+            push!(allLayers, Layer(neurons, 0.0))
         end
         oneWeights = Weights((0, 1), Dict{Tuple{Int64,Int64},Float64}())
         for i = 1:inputAmount
@@ -88,25 +84,17 @@
     end
 
 #Constants
-    const ϵ = 0.01 # the learning rate
+    const ϵ = 0.005 # the learning rate
 
 #Mathematical functions
-    function sig(x::Number) #the activision function; it takes a number and returns the output of the sigmoid-function for it
+    function sig(x) #the activision function; it takes a number and returns the output of the sigmoid-function for it
         return 1 / (1 + ℯ^-x)
     end
-    #=
-    function sig(x::AbstractArray) #the activision function; it takes a number and returns the output of the sigmoid-function for it
-        return map(y -> 1 / (1 + ℯ^-y), x)
-    end
-    =#
-    function δSig(x::Number) #the derivative of the activision function
+
+    function δSig(x) #the derivative of the activision function
         return sig(x) * (1 - sig(x))
     end
-    #=
-    function δSig(x::AbstractArray) #the derivative of the activision function
-        return sig(x) .* (1 .- sig(x))
-    end
-    =#
+
 #Visualization functions
     function printNetworkStructure(netti)
         println("The input layer: ")
@@ -150,36 +138,31 @@
 #Network functions
 
 function getNumbers(string::String, divider::AbstractString)
-    # get numbers from a string which are seperated by $divider
+    
 end
 
 function saveNetwork(path, network)
-    open(path, write=true) do io
-        JSON.print(io, JSON.json(network))
-    end
+    save(path, "network", network)
+end
+
+function savePlot(path, x, y)
+    save(path, "x", x, "y", y)
 end
 
 function loadNetwork(path)
-    io = open(path)
-    output = JSON.parse(JSON.parse(io))
-    close(io)
-    allInputs = output["allInputs"]
-    allLayers = map(x -> Layer(map(y -> Neuron(y["netinput"], y["activision"], y["δ"]), x["neurons"]), x["bias"]), output["allLayers"])
-    allWeights = map(x -> Weights(x["betweenLayers"], Dict(map(y -> y[1], collect(x["weights"])))), output["allWeights"])
-    Network()
-    return output
+    return load(path, "network")
 end
 
 function forwardPass(network, inputs=nothing)
     if inputs !== nothing
-        setInputs(network, inputs)
+        network = setInputs(network, inputs)
     end
     for i = 1:length(network.allLayers[1].neurons)
         sum = 0
         for ii = 1:length(network.allInputs)
             sum += network.allInputs[ii] * network.allWeights[1].weights[(ii, i)]
         end
-        sum += network.allLayers[1].neurons[i].bias
+        sum += network.allLayers[1].bias
         network.allLayers[1].neurons[i].netinput = sum
         network.allLayers[1].neurons[i].activision = sig(sum)
     end
@@ -189,7 +172,7 @@ function forwardPass(network, inputs=nothing)
             for i3 = 1:length(network.allLayers[i-1].neurons)
                 sum += network.allLayers[i-1].neurons[i3].activision * network.allWeights[i].weights[(i3, ii)]
             end
-            sum += network.allLayers[i].neurons[ii].bias
+            sum += network.allLayers[i].bias
             network.allLayers[i].neurons[ii].netinput = sum
             network.allLayers[i].neurons[ii].activision = sig(sum)
         end
@@ -205,33 +188,27 @@ function setInputs(network, inputs)
     return network
 end
 
-function δ(layer::Int, posInLayer::Int, network, supposedOutputs::Array{Float64, 1})
+function δ(layer::Int, posInLayer::Int, network, supposedOutputs::Array{Any, 1}) # supposedOutputs::Array{Float64, 1}
     if layer == length(network.allLayers)
-        result = δSig(network.allLayers[layer].neurons[posInLayer].netinput) * (supposedOutputs[posInLayer] - network.allLayers[layer].neurons[posInLayer].activision), 
-                 δSig(1) * (supposedOutputs[posInLayer] - network.allLayers[layer].neurons[posInLayer].activision) # *2 ? (s. https://youtu.be/tIeHLnjs5U8?list=PL_h2yd2CGtBHEKwEH5iqTZH85wLS-eUzv&t=348)
+        result = δSig(network.allLayers[layer].neurons[posInLayer].netinput) * (supposedOutputs[posInLayer] - network.allLayers[layer].neurons[posInLayer].activision)
     else
         sum = 0.0
         for i = 1:length(network.allLayers[layer+1].neurons)
             sum += network.allLayers[layer+1].neurons[i].δ * network.allWeights[layer+1].weights[(posInLayer, i)]
         end
-        result = sum * δSig(network.allLayers[layer].neurons[posInLayer].netinput),
-                 sum * δSig(1)
+        result = sum * δSig(network.allLayers[layer].neurons[posInLayer].netinput)
     end
     return result
 end
 
 # layer1 and posInLayer1 for the first Neuron (forward-pass), layer2 and posInLayer2 for the second Neuron (forward-pass)
-function ΔWb(layer1::Int, posInLayer1::Int, layer2::Int, posInLayer2::Int, network::Network, ϵ::Float64, supposedOutputs::Array{Float64, 1})
-    network.allLayers[layer2].neurons[posInLayer2].δ, δb = δ(layer2, posInLayer2, network, supposedOutputs)
+function ΔW(layer1::Int, posInLayer1::Int, layer2::Int, posInLayer2::Int, network::Network, ϵ::Float64, supposedOutputs::Array{Any, 1}) # supposedOutputs::Array{Float64, 1}
+    network.allLayers[layer2].neurons[posInLayer2].δ = δ(layer2, posInLayer2, network, supposedOutputs)
     if layer1 == 0
-        return (network, ϵ * network.allLayers[layer2].neurons[posInLayer2].δ * network.allInputs[posInLayer1], δb)
+        return (network, ϵ * network.allLayers[layer2].neurons[posInLayer2].δ * network.allInputs[posInLayer1])
     else
-        return (network, ϵ * network.allLayers[layer2].neurons[posInLayer2].δ * network.allLayers[layer1].neurons[posInLayer1].activision, δb)
+        return (network, ϵ * network.allLayers[layer2].neurons[posInLayer2].δ * network.allLayers[layer1].neurons[posInLayer1].activision)
     end
-end
-
-function Δb(layer::Int, network::Network)
-    return ϵ * δSig(map(x -> x.bias, network.allLayers[layer].neurons)) * 2 * ()
 end
 
 function backpropagation(network::Network, training_data::TrainingData, ϵ::Float64)
@@ -242,11 +219,10 @@ function backpropagation(network::Network, training_data::TrainingData, ϵ::Floa
         for j = -length(network.allLayers):-1
             arrayWeights = map(x -> (x[1], x[2]), collect(network.allWeights[-j].weights))
             for k = 1:length(arrayWeights)
-                network2, outputΔW, outputΔb = ΔWb(-j-1, arrayWeights[k][1][1], -j, arrayWeights[k][1][2], network2, ϵ, training_data.outputs[i])
+                network2, outputΔW = ΔW(-j-1, arrayWeights[k][1][1], -j, arrayWeights[k][1][2], network2, ϵ, training_data.outputs[i])
                 network2.allWeights[-j].weights[arrayWeights[k][1]] += outputΔW
-                network2.allLayers[-j].neurons[arrayWeights[k][1][2]].bias += outputΔb
             end
-            #network2.allLayers[-j].neurons[] = ΔW(-j, ) # !!!!!!!!
+            network2.allLayers[-j].bias = network2.allLayers[-j].bias # !!!!!!!!
         end
     end
     return network2
@@ -260,14 +236,40 @@ function bitArrayFromInt(number::Int, bit_digits)
     return arr
 end
 
-function generateTrainingData(min::Int, max::Int, input_amount::Int) #checking whether dividable by 2
-    training_data = TrainingData([], [])
-    for i = min:max
-        arr = bitArrayFromInt(i, input_amount)
-        push!(training_data.inputs, arr)
-        push!(training_data.outputs, mod(i, 3) == 0 ? [1.0] : [0.0])
+function generateTrainingData(location::String) #checking whether dividable by 2
+    io = open(location, "r")
+    content = read(io, String)
+    close(io)
+    content = split(content,":")
+    foo = []
+    for i = 1:length(content)-1
+        push!(foo, (split(content[i], ";")[1],
+                    split(content[i], ";")[2])
+             )
     end
-    return training_data
+    content = foo
+    foo = []
+    for i = 1:length(content)
+        push!(foo, (split(content[i][1], ","),
+                    split(content[i][2], ",")
+              ))
+    end
+    content = foo
+    foo = []
+    for i = 1:length(content)
+        push!(foo, ([], []))
+        for i2 = 1:length(content[i][1])-1
+            push!(foo[i][1], parse(Float32, String(content[i][1][i2])))
+        end
+        for i3 = 1:length(content[i][2])-1
+            push!(foo[i][2], parse(Float32, String(content[i][2][i3])))
+        end
+    end
+    content = foo
+    println(typeof(content))
+    content = TrainingData(map(x -> x[1], content), map(x -> x[2], content))
+    println(length(content.outputs), "\n", length(content.inputs))
+    return content
 end
 
 function checkAccuracy(network::Network, training_data::TrainingData)
@@ -297,14 +299,15 @@ end
 #menu = "load"
 menu = "create"
 if menu == "create"
+    println("Defining training data...")
+    training_data = generateTrainingData("myfile.txt")
+    println("Training data defined.")
     println("Creating network structure...")
-    network = createNetworkStructure(10, [10, 9, 3, 1], true)
+    network = createNetworkStructure(length(training_data.inputs[1]), [20, 20, 20, 15, 15, 3, length(training_data.outputs[1])], true)
+    printNetworkStructure(network)
     #network = createNetworkStructure(100, [200, 1000, 2000, 1000, 100, 99, 10, 1], true) end
     #setInputs(network, rand(-100:100, 10))
     println("Network structure created.")
-    println("Defining training data...")
-    training_data = generateTrainingData(1, 100, 10)
-    println("Training data defined.")
     network = forwardPass(network)
     costs = []
     println("Doing backpropagations -> \"learning\"...")
@@ -318,14 +321,15 @@ if menu == "create"
         updateProgress(i, 100)
     end
     println("Backpropagations done.")
-    filepath = "saves\\network4.json"
+    filepath = "saves\\network6.jld"
     println("Saving network at $filepath...")
     saveNetwork(filepath, network)
     println("Network saved.")
-    printNetworkStructure(network)
     checkAccuracy(network, training_data)
     println("Cost: ", getCost(network, training_data))
     x = [i for i = 1:length(costs)]
+    filepath = "saves\\plot1.jld"
+    savePlot("saves\\plot1.jld", x, costs)
     plot(x, costs)
     input = ""
     while input != "exit"
@@ -345,7 +349,7 @@ if menu == "create"
         end
     end
 elseif menu == "load"
-    filepath = "saves\\network1.json"
+    filepath = "saves\\network6.jld"
     network = loadNetwork(filepath)
     printNetworkStructure(network)
     checkAccuracy(network, training_data)
